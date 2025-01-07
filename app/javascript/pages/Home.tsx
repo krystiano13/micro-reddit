d4632a6ac0d63767d37f95448f1a9daff6068934
krystiano13 <crystianotv@gmail.com>
2025-01-07 17:31:27 +0100

    implement sharing user data to inertia views

--- app/javascript/pages/Home.tsx
+++ app/javascript/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { Head } from '@inertiajs/react'
-import { Input } from '@mantine/core';
+import { useEffect } from "react";
 
-export default function Home({ name }: { name: string }) {
-  return (
-    <>
-      <Head title="Inertia + Vite Ruby + React Example" />
-      <Input placeholder="Test" type="text" />
-    </>
-  )
+export default function Home({ user }: { user: any }) {
+    useEffect(() => {
+        console.log(user);
+    }, []);
+
+      return (
+        <>
+          <Head title="REDDIT:RE" />
+        </>
+      )
 }
