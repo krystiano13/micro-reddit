25c5f5dbe8b6bc02cd922b82a44779d636986e6c
krystiano13 <crystianotv@gmail.com>
2025-01-07 21:52:01 +0100

    implement omniauth logic in rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
