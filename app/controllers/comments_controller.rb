50933b707c5ce12bd0594d8187f66672c205ba9c
krystiano13 <crystianotv@gmail.com>
2025-01-14 14:40:55 +0100

    add error handling for creating comments on backend

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,22 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     @comment.user_id = current_user.id
-    @comment.save
 
-    redirect_back(fallback_location: root_path)
+    if @comment.save
+      redirect_to post_path(id: params[:post_id]), inertia: {
+        notice: 'Comment was successfully created.'
+      }
+    else
+      redirect_to post_path(id: params[:post_id]), inertia: {
+        errors: @comment.errors.full_messages
+      }
+    end
   end
 
   def update
-
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
